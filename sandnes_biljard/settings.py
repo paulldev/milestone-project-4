1f98bd9b39db458438f9e53c7ba006f10f5b11e9
Paul <paulinnorway@gmail.com>
2020-10-24 15:56:28 +0000

    Add static files in settings.py

--- sandnes_biljard/settings.py
+++ sandnes_biljard/settings.py
@@ -146,3 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',) #must be a tuple
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
