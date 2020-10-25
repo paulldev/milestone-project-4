a9173b0bd5a1b6d6712524832fe9f166a9df562e
Paul <paulinnorway@gmail.com>
2020-10-25 13:56:34 +0000

    Create context processor

---
+++ cart/contexts.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+    
+    grand_total = delivery + total
+    
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
+    }
+
+    return context
