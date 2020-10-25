469a9eb4134bed3f6033f6dcb4303ed2af2f713e
Paul <paulinnorway@gmail.com>
2020-10-25 17:50:36 +0000

    Add add/<item_id> to urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_cart, name='view_cart')
+    path('', views.view_cart, name='view_cart'),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
