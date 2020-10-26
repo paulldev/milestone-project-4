d9b3246c1ca9252613e22fd5a0ac3375d606b55e
Paul <paulinnorway@gmail.com>
2020-10-26 09:07:11 +0000

    Add OrderLineItemAdminInline & OrderAdmin models. Register both.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date', 'total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'total',)
+
+    list_display = ('order_number', 'date', 'full_name', 'total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
