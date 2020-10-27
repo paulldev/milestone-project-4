989b8e3058dc49d2355f87cbe75a9ddc8ab97cb3
Paul <paulinnorway@gmail.com>
2020-10-27 13:14:16 +0000

    Add Stripe test keys to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HgZsDANqxLd0au22EpGRpjdTtumRvkYzBrgEzayerMn6v4TBRYOHIYAV6a9NCRFgGboOXtOp0os85YGfturFKWq008hviwjBp',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
