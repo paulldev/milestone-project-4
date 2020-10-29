0a7c800fc5f05fd08d80928f0178efdd01c677a5
Paul <paulinnorway@gmail.com>
2020-10-29 08:07:58 +0000

    Add eu-north-1 as region name

--- sandnes_biljard/settings.py
+++ sandnes_biljard/settings.py
@@ -172,7 +172,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'ci-milestoneproject-4'
-    AWS_S3_REGION_NAME = 'eu-stockholm'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
