891902480c0adab515392ed0d67426d87e46534e
Paul <paulinnorway@gmail.com>
2020-10-30 22:14:12 +0000

    Define feedback_view

--- feedback/views.py
+++ feedback/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-from .models import PoolTable
+from django.shortcuts import render, HttpResponse
+from .models import PoolTable, Review
+from .forms import TableForm, ReviewForm
+
 # Create your views here.
 
+
 def feedback_view(request):
-    pool_table_objects = PoolTable.objects.all()
-    context = {
-        'pool_table_objects': pool_table_objects
-    }
+    if request.method == 'POST':
+        table_form = TableForm(request.POST)
+        review_form = ReviewForm(request.POST)
+
+        if table_form.is_valid() and review_form.is_valid():
+            table_form.save()
+            review_form.save()
+            return HttpResponseRedirect('/success')        
+        else:
+            context = {
+                'table_form': table_form,
+                'review_form': review_form,
+            }
+    else:
+        context = {
+            'table_form': TableForm(),
+            'review_form': ReviewForm(),
+        }
     return render(request, "feedback/feedback.html", context)
