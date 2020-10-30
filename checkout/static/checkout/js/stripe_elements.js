9ee6bcfd1e44ccefe7160d3c725a37908c1875b9
Paul <paulinnorway@gmail.com>
2020-10-30 16:07:34 +0000

    Fix overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -50,8 +50,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true}); //prevent multiple submissions
     $('#submit-button').attr('disabled', true);
-//    $('#payment-form').fadeToggle(100);
-//    $('#loading-overlay').fadeToggle(100);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
 
     var saveInfo = Boolean($('#id-save-info').attr('checked'));
     // From using {% csrf_token %} in the form
@@ -84,8 +84,8 @@ form.addEventListener('submit', function(ev) {
                     </span>
                     <span>${result.error.message}</span>`;
                 $(errorDiv).html(html);
-//                $('#payment-form').fadeToggle(100);
-//                $('#loading-overlay').fadeToggle(100);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
                 card.update({ 'disabled': false});
                 $('#submit-button').attr('disabled', false);
             } else {
