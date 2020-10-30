3484b2c974e06aaae94a86f7673fbf1bc352002a
Paul <paulinnorway@gmail.com>
2020-10-30 19:34:51 +0000

    Create feedback_view

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import PoolTable
 # Create your views here.
+
+def feedback_view(request):
+    pool_table_objects = PoolTable.objects.all()
+    context = {
+        'pool_table_objects': pool_table_objects
+    }
+    return render(request, "feedback/feedback.html", context)
