0909e0c471fe30ef3ba096e9f30b1698b955eb68
Paul <paulinnorway@gmail.com>
2020-10-30 19:33:58 +0000

    Create PoolTable, Review models

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class PoolTable(models.Model):
+    table_number = models.IntegerField()
+    clean_rating = models.IntegerField()
+    damage_rating = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.table_number}'
+
+
+class Review(models.Model):
+    review_text = models.TextField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.rating}'
