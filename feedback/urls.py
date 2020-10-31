75ab1bb2005bbd27b552918750b43a901423f13c
Paul <paulinnorway@gmail.com>
2020-10-31 09:02:12 +0000

    Add feedback_received url

--- feedback/urls.py
+++ feedback/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.feedback_view, name='feedback_view'),
+    path('feedback_received', views.feedback_received, name='feedback_received'),
+
 ]
