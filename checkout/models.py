5e4ca9ee9521b6c0953f03d97d70867113813c47
Paul <paulinnorway@gmail.com>
2020-10-31 11:15:30 +0000

    Add order_total

--- checkout/models.py
+++ checkout/models.py
@@ -14,7 +14,8 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=20, null=True, blank=True)
     table_number = models.IntegerField(null=False, default=1)
     date = models.DateTimeField(auto_now_add=True)
-    total = models.IntegerField(null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_cart = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
        
@@ -28,7 +29,8 @@ class Order(models.Model):
         """
         Update total each time a line item is added
         """
-        self.total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.total = self.order_total
         self.save()
 
     def save(self, *args, **kwargs):
